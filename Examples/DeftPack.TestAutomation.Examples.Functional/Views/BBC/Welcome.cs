6c226b070fc4ba08ebd0ba8914005ebc54dc7c00
Zoltan Simari <simari.zoltan@gmail.com>
2014-10-19 23:28:07 +0100

    Adding views to the example

---
+++ Examples/DeftPack.TestAutomation.Examples.Functional/Views/BBC/Welcome.cs
@@ -0,0 +1,17 @@
+﻿using DeftPack.TestAutomation.Selenium.PageObjects;
+using DeftPack.TestAutomation.Selenium.PageObjects.Elements;
+
+namespace DeftPack.TestAutomation.Examples.Functional.Views.BBC
+{
+    public class Welcome : View
+    {
+        public Link WeatherLink
+        {
+            get
+            {
+                return Elements.Query<Link>(
+                        x => x.WithText("Weather").ChildOf(Elements.Tag("div").WithCssClass("orb-nav-section")));
+            }
+        }
+    }
+}
